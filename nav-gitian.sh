ffec613e9488e103ee2ce7e17cb81d23b3cb1d1c
Craig M <craig.b.macgregor@gmail.com>
2019-07-30 20:01:19 +1200

    Update nav-gitian.sh

--- nav-gitian.sh
+++ nav-gitian.sh
@@ -1,82 +1,76 @@
 #!/bin/sh
-
+  
 # example
-# ./build.sh https://github.com/NavCoin/navcoin-core.git master {X86|OSX|WIN|ARM|ALL}
+# ./build.sh https://github.com/NavCoin/navcoin-core.git master ALL
 
-export VERSION=$2
-export URL=$1
-export USE_LXC=1
-export GITIAN_HOST_IP=10.0.3.2
-export LXC_GUEST_IP=10.0.3.5
+# $DIR is the absolute path of where you cloned gitian-builder
 
+DIR=/home/navworker
+
+VERSION=$2
+URL=$1
 OS=$3
 
 if [ "$1" = "" ]; then
-	echo "Usage $0 {repository} {branch|tag|commit} {X86|OSX|WIN|ARM|ALL}"
-	exit 1
+        echo "Usage $0 {repository} {branch|tag|commit} {LINUX|OSX|WIN|ALL}"
+        exit 1
 fi
 
 if [ "$2" = "" ]; then
-	echo "Usage $0 $1 {branch|tag|commit} {X86|OSX|WIN|ARM|ALL}"
-	exit 1
+        echo "Usage $0 $1 {branch|tag|commit} {LINUX|OSX|WIN|ALL}"
+        exit 1
 fi
 
 case "$OS" in
-	X86)
-		echo "Building for x86 and Aarch Linux"
-		;;
-	OSX)
-		echo "Building for Mac OSX"
-		;;
-	WIN)
-		echo "Building for 32bit and 64bit Windows"
-		;;
-	ARM)
-		echo "Building for Arm and i686 Linux"
-		;;
-	ALL)
-		echo "Building for all compatible systems"
-		;;
-	*)
-		echo "Usage $0 $1 $2 $3 {X86|OSX|WIN|ARM|ALL}"
-		exit 1
+        LINUX)
+                echo "Building for x86, aarch, i686 and arm Linux"
+                ;;
+        OSX)
+                echo "Building for Mac OSX"
+                ;;
+        WIN)
+                echo "Building for 32bit and 64bit Windows"
+                ;;
+        ALL)
+                echo "Building for all compatible systems"
+                ;;
+        *)
+                echo "Usage $0 $1 $2 $3 {LINUX|OSX|WIN|ALL}"
+                exit 1
 esac
 
-rm  -rf /home/debian/navcoin-core && cd /home/debian && git clone $URL
- 
-cd /home/debian/navcoin-core && git pull ; git checkout $VERSION ; git pull
- 
-mkdir -p /home/debian/binaries/$VERSION
+rm  -rf $DIR/navcoin-core && cd $DIR && git clone $URL
 
-if [ "$OS" = "X86" ] || [ "$OS" = "ALL" ] ; then
-	cd /home/debian/gitian-builder && ./bin/gbuild --memory 7000 -j 2 --commit navcoin-core=${VERSION} --url navcoin-core=${URL} /home/debian/navcoin-core/contrib/gitian-descriptors/gitian-linux.yml
-	mv build/out/navcoin-*.tar.gz build/out/src/navcoin-*.tar.gz /home/debian/binaries/$VERSION/
-	cd /home/debian/binaries/$VERSION && rm *SHA256SUM* ; sha256sum * > $VERSION.SHA256SUM.asc
+cd $DIR/navcoin-core ; git pull ; git checkout $VERSION ; git pull
+
+mkdir -p $DIR/binaries/$VERSION
+
+if [ "$OS" = "LINUX" ] || [ "$OS" = "ALL" ] ; then
+        echo "$OS $VERSION $URL"
+        cd $DIR/gitian-builder && USE_DOCKER=1 ./bin/gbuild -j 6 --commit navcoin-core=${VERSION} --url navcoin-core=${URL} $DIR/navcoin-core/contrib/gitian-descriptors/gitian-linux.yml
+        mv build/out/navcoin-*.tar.gz build/out/src/navcoin-*.tar.gz $DIR/binaries/$VERSION/
+        cd $DIR/binaries/$VERSION && rm *SHA256SUM* ; sha256sum * > $VERSION.SHA256SUM.asc
 fi
 
 if [ "$OS" = "OSX" ] || [ "$OS" = "ALL" ] ; then
-	cd /home/debian/gitian-builder && ./bin/gbuild --memory 7000 -j 2 --commit navcoin-core=${VERSION} --url navcoin-core=${URL} /home/debian/navcoin-core/contrib/gitian-descriptors/gitian-linux.yml
-	cd /home/debian/gitian-builder && ./bin/gbuild --memory 7000 -j 2 --commit navcoin-core=${VERSION} --url navcoin-core=${URL} /home/debian/navcoin-core/contrib/gitian-descriptors/gitian-osx.yml
-	mv build/out/navcoin-*-osx.tar.gz inputs/navcoin-osx.tar.gz
-	mv build/out/navcoin-*.tar.gz build/out/navcoin-*.dmg /home/debian/binaries/$VERSION/
-	cd /home/debian/binaries/$VERSION && rm *SHA256SUM* ; sha256sum * > $VERSION.SHA256SUM.asc
+        if [ "$OS" = "OSX" ] ; then
+                cd $DIR/gitian-builder && USE_DOCKER=1 ./bin/gbuild -j 6 --commit navcoin-core=${VERSION} --url navcoin-core=${URL} $DIR/navcoin-core/contrib/gitian-descriptors/gitian-linux.yml
+        fi
+        cd $DIR/gitian-builder && USE_DOCKER=1 ./bin/gbuild -j 6 --commit navcoin-core=${VERSION} --url navcoin-core=${URL} $DIR/navcoin-core/contrib/gitian-descriptors/gitian-osx.yml
+        mv build/out/navcoin-*-osx.tar.gz inputs/navcoin-osx.tar.gz
+        mv build/out/navcoin-*.tar.gz build/out/navcoin-*.dmg $DIR/binaries/$VERSION/
+        cd $DIR/binaries/$VERSION && rm *SHA256SUM* ; sha256sum * > $VERSION.SHA256SUM.asc
 fi
 
 if [ "$OS" = "WIN" ] || [ "$OS" = "ALL" ] ; then
-	cd /home/debian/gitian-builder && ./bin/gbuild --memory 7000 -j 2 --commit navcoin-core=${VERSION} --url navcoin-core=${URL} /home/debian/navcoin-core/contrib/gitian-descriptors/gitian-linux.yml
-	cd /home/debian/gitian-builder && ./bin/gbuild --memory 7000 -j 2 --commit navcoin-core=${VERSION} --url navcoin-core=${URL} /home/debian/navcoin-core/contrib/gitian-descriptors/gitian-win.yml
-	mv build/out/navcoin-*-win.tar.gz inputs/navcoin-win.tar.gz
-	mv build/out/navcoin-*.zip build/out/navcoin-*.exe /home/debian/binaries/$VERSION/
+        if [ "$OS" = "WIN" ] ; then
+                cd $DIR/gitian-builder && USE_DOCKER=1 ./bin/gbuild -j 6 --commit navcoin-core=${VERSION} --url navcoin-core=${URL} $DIR/navcoin-core/contrib/gitian-descriptors/gitian-linux.yml
+        fi
+        cd $DIR/gitian-builder && USE_DOCKER=1 ./bin/gbuild -j 6 --commit navcoin-core=${VERSION} --url navcoin-core=${URL} $DIR/navcoin-core/contrib/gitian-descriptors/gitian-win.yml
+        mv build/out/navcoin-*-win.tar.gz inputs/navcoin-win.tar.gz
+        mv build/out/navcoin-*.zip build/out/navcoin-*.exe $DIR/binaries/$VERSION/
 fi
 
-if [ "$OS" = "ARM" ] || [ "$OS" = "ALL" ] ; then
-	cd /home/debian/gitian-builder && ./bin/gbuild --memory 7000 -j 2 --commit navcoin-core=${VERSION} --url navcoin-core=${URL} /home/debian/navcoin-core/contrib/gitian-descriptors/gitian-linux.yml
-	cd /home/debian/gitian-builder && ./bin/gbuild --memory 3000 -j 2 --commit navcoin-core=${VERSION} --url navcoin-core=${URL} /home/debian/navcoin-core/contrib/gitian-descriptors/gitian-arm.yml
-	mv build/out/navcoin-*.tar.gz build/out/src/navcoin-*.tar.gz /home/debian/binaries/$VERSION/
-	cd /home/debian/binaries/$VERSION && rm *SHA256SUM* ; sha256sum * > $VERSION.SHA256SUM.asc
-fi
-
-cd /home/debian/binaries/$VERSION && rm *SHA256SUM* ; sha256sum * > $VERSION.SHA256SUM.asc
- 
-#rm /home/blanka/binaries/$VERSION/*debug*
-#cd /home/blanka/binaries/$VERSION && rm *SHA256SUM* ; sha256sum * > $VERSION.SHA256SUM.asc
+cd $DIR/binaries/$VERSION && rm *SHA256SUM* ; sha256sum * > $VERSION.SHA256SUM.asc
+rm $DIR/binaries/$VERSION/*debug*
+cd $DIR/binaries/$VERSION && rm *SHA256SUM* ; sha256sum * > $VERSION.SHA256SUM.asc
