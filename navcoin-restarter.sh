4e6ef8b5d80d3011c7c1c2edec4f28803b4746a0
Craig M <craig.b.macgregor@gmail.com>
2021-07-01 13:27:25 +1200

    Create navcoin-restarter.sh

---
+++ navcoin-restarter.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+echo $(date -u) "stopping navcoind"
+
+/<path>/navcoin-cli stop
+
+SLEEP1=0
+while [ "$SLEEP1" -lt 6 ]; do
+	STOP_ERROR=$(/<path>/navcoin-cli getinfo 2>&1 >/dev/null)
+	if [ "$STOP_ERROR" == "error: couldn't connect to server" ]; then
+		echo "shutdown successful"
+		SLEEP1=6
+	else	
+		echo "waiting for shutdown..."
+		SLEEP1=$[$SLEEP1+1]
+		sleep 10s
+	fi
+done
+
+echo $(date -u) "starting navcoind"
+
+/<path>/navcoind &
+
+SLEEP2=0
+while [ "$SLEEP2" -lt 30 ]; do
+	START=$(/<path>/navcoin-cli getinfo | jq -r '.testnet')
+        if [ "$START" == "false" ]; then
+		echo "startup successful"
+                SLEEP2=30
+        else    
+                echo "waiting for startup..." 
+                SLEEP2=$[$SLEEP2+1]
+                sleep 30s
+        fi
+
+done
+
+echo $(date -u) "unlocking for staking"
+
+/<path>/navcoin-cli walletpassphrase '<password>' 999999999 true
+
+SLEEP3=0
+while [ "$SLEEP3" -lt 6 ]; do
+        STAKING=$(/<path>/navcoin-cli signmessage <address> 'test' 2>&1 >/dev/null | sed -n '3 p')
+        if [ "${STAKING}" == "Error: Wallet is unlocked for staking or mixing only." ]; then
+                echo "wallet is unlocked for staking"
+		SLEEP3=6
+        else
+                echo "testing staking..."
+                SLEEP3=$[$SLEEP3+1]
+                sleep 10s
+        fi
+done
+
+
+echo $(date -u) "restarted complete"
